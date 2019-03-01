63ed437af74db68c276346a1a8f026a9962f152f
bconnelly <tug21498@temple.edu>
2019-03-01 14:58:12 -0500

    added Ndef Message callback implementation

--- app/src/main/java/edu/temple/nfccomponent/MainActivity.java
+++ app/src/main/java/edu/temple/nfccomponent/MainActivity.java
@@ -4,13 +4,26 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcEvent;
+import android.nfc.tech.NfcA;
 import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NfcAdapter.CreateNdefMessageCallback {
     KeyService keyService;
     boolean connected;
+    String userKeyForExchange = null;
+
+    NfcAdapter nfcAdapter;
 
     ServiceConnection serviceConnection = new ServiceConnection() {
         @Override
@@ -20,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
             connected = true;
 
             keyService.getMyKeyPair();
-            keyService.getUserPublicForExchange("brendan");
+            userKeyForExchange = keyService.getUserPublicForExchange("brendan");
         }
 
         @Override
@@ -33,6 +46,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (nfcAdapter == null) {
+            Toast.makeText(this, "NFC is not available on this device.", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        nfcAdapter.setNdefPushMessageCallback(this, this );
     }
 
     @Override
@@ -47,4 +69,12 @@ public class MainActivity extends AppCompatActivity {
         super.onStop();
         unbindService(serviceConnection);
     }
+
+    @Override
+    public NdefMessage createNdefMessage(NfcEvent nfcEvent) {
+        String messageString = userKeyForExchange;
+        NdefRecord ndefRecord = NdefRecord.createMime("text/plain", messageString.getBytes());
+        return new NdefMessage(ndefRecord);
+    }
+
 }
